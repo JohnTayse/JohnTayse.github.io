55eeaeb4236b396d0f4401f77f0160b6014aba9a
John Tayse <jctayse@gmail.com>
2021-02-03 18:07:01 -0500

    Removed Reddit Browser
    Moved to own repo

--- RedditBrowser/main.js
+++
@@ -1,7 +0,0 @@
-import { router } from './modules/router.js';
-
-window.addEventListener('hashchange', router);
-window.addEventListener('load', router);
-if(window.location.hash === ''){
-	window.location = '#';
-}
